57c9f61825df4b5fe89bf38efb3a8c0639c34d1f
Arthur Flam <arthur.flam@gmail.com>
2015-12-21 19:48:52 +0200

    added ==== comments to improve readability

--- app.py
+++ app.py
@@ -5,30 +5,27 @@ from tokenizers import my_tokenizer
 
 
 # ===========================================================================
-
-# ===========================================================================
-# TODO : a class for documents
-# - will enable get_text rather than doc["Name"]
+# TODO :
+# - a class for documents will enable doc.get_text() rather than doc["Name"]
 # ===========================================================================
 
 def json_loader(filepath = "products.json"):
-    """ returns an array of documents """
-    """ it would be better if it returned an iterator (large files / DB) """
+    """ returns an array of documents read from the JSON file """
+    """ other function will work as long as they return iterable types """
     file = open(filepath,'r', encoding="utf-8")
     return json.loads(file.read(), encoding="utf-8")
 
-# ===========================================================================
-
+# == INIT ===================================================================
 tfidf_data = TermDocumentMatrixDD(json_loader, my_tokenizer)
 
 
+# === SERVER ================================================================
 from flask import Flask, request, render_template
 app = Flask(__name__)
 @app.route("/")
 def search():
     query = request.args.get('q', '')
     return render_template('search.html', results=tfidf_data.search(query))
-
 
 if __name__ == "__main__":
     app.run(debug=True)
