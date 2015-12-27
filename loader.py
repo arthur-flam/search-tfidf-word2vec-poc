97c6add7c2cf9f49f3adc9ecb3a25707e3dc6b12
Arthur Flam <arthur@lookies.io>
2015-12-27 16:11:57 +0000

    moved loader files to their own location

---
+++ loader.py
@@ -0,0 +1,19 @@
+"""
+Exports useful data loaders
+"""
+from pymongo import MongoClient
+import settings
+
+def LOADER_JSON(filepath="products.json"):
+    """
+    Returns an array of documents read from the JSON file
+    Other function will work as long as they return iterable types
+    """
+    file_connection = open(filepath, 'r', encoding="utf-8")
+    return json.loads(file_connection.read(), encoding="utf-8")
+
+def LOADER_MONGODB():
+    client = MongoClient('localhost', 27017)
+    print("connecting to "+ settings.DB_NAME)
+    products = client[settings.DB_NAME].products
+    return products.find(limit=0)
